acc4bb0bd3d2a75fb09e78fa64b83d08290fffc6
georgemoralis <giorgosmrls@gmail.com>
2025-05-15 09:31:13 +0300

    clang format

--- src/emulator.h
+++ src/emulator.h
@@ -21,10 +21,10 @@ public:
 private:
     void LoadSystemModules(const std::string& game_serial);
 
-    //Core::MemoryManager* memory;
-    //Input::GameController* controller;
-    //Core::Linker* linker;
-    //std::unique_ptr<Frontend::WindowSDL> window;
+    // Core::MemoryManager* memory;
+    // Input::GameController* controller;
+    // Core::Linker* linker;
+    // std::unique_ptr<Frontend::WindowSDL> window;
     std::chrono::steady_clock::time_point start_time;
 };
 
