65ad564386ad51960999a45e62908e7903568834
georgemoralis <giorgosmrls@gmail.com>
2025-09-18 23:05:09 +0300

    New translations en_us.ts (Italian)

--- src/qt_gui/translations/it_IT.ts
+++ src/qt_gui/translations/it_IT.ts
@@ -2422,11 +2422,11 @@
     </message>
     <message>
       <source>Audio Device (general)</source>
-      <translation type="unfinished">Audio Device (general)</translation>
+      <translation>Dispositivo Audio (Generale)</translation>
     </message>
     <message>
       <source>Audio Device (DS4 speaker)</source>
-      <translation type="unfinished">Audio Device (DS4 speaker)</translation>
+      <translation>Dispositivo Audio (altoparlante DS4)</translation>
     </message>
   </context>
   <context>
