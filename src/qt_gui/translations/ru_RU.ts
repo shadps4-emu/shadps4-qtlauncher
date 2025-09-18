204eff7d0571ffffbc84dc35dd9c05d0ef5508a3
georgemoralis <giorgosmrls@gmail.com>
2025-09-19 00:23:41 +0300

    New translations en_us.ts (Russian)

--- src/qt_gui/translations/ru_RU.ts
+++ src/qt_gui/translations/ru_RU.ts
@@ -2428,6 +2428,14 @@
       <source>Audio Device (DS4 speaker)</source>
       <translation>Аудио устройство (динамик DS4)</translation>
     </message>
+    <message>
+      <source>shadPS4 Path</source>
+      <translation type="unfinished">shadPS4 Path</translation>
+    </message>
+    <message>
+      <source>Select the shadPS4 executable</source>
+      <translation type="unfinished">Select the shadPS4 executable</translation>
+    </message>
   </context>
   <context>
     <name>TrophyViewer</name>
