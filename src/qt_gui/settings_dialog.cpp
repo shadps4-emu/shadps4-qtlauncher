b4aa8db0b5688fc081c53371aa1c3d5ab239ade4
Fire Cube <ben7@gmx.ch>
2025-09-21 14:59:46 +0200

    bump required Vulkan verision (#12)

--- src/qt_gui/settings_dialog.cpp
+++ src/qt_gui/settings_dialog.cpp
@@ -1284,11 +1284,11 @@ void SettingsDialog::getPhysicalDevices() {
     // Create Vulkan instance
     VkApplicationInfo appInfo{};
     appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
-    appInfo.pApplicationName = "shadPS4 launcher";
+    appInfo.pApplicationName = "shadPS4QtLauncher";
     appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
     appInfo.pEngineName = "No Engine";
     appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
-    appInfo.apiVersion = VK_API_VERSION_1_0;
+    appInfo.apiVersion = VK_API_VERSION_1_3;
 
     VkInstanceCreateInfo instInfo{};
     instInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
