0188f1a4cc1f81464ff93900ad3e787767fce343
Dzmitry Dubrova <dimaxdqwerty@gmail.com>
2025-09-22 17:14:23 +0300

    Fix clang

--- src/qt_gui/main_window.cpp
+++ src/qt_gui/main_window.cpp
@@ -1235,7 +1235,9 @@ void MainWindow::StartEmulator(std::filesystem::path path) {
     QString exe = m_gui_settings->GetValue(gui::gen_shadPath).toString();
     QFileInfo fileInfo(exe);
     if (!fileInfo.exists()) {
-        QMessageBox::critical(nullptr, tr("ShadPS4"), QString(tr("ShadPS4 is not found!\nPlease change ShadPS4 path in settings.")));
+        QMessageBox::critical(
+            nullptr, tr("ShadPS4"),
+            QString(tr("ShadPS4 is not found!\nPlease change ShadPS4 path in settings.")));
         return;
     }
 
