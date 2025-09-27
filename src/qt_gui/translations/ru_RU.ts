73e33e9fa3a1735dc07d8dd6971342a21a0e18a7
georgemoralis <giorgosmrls@gmail.com>
2025-09-27 11:33:41 +0300

    New translations en_us.ts (Russian)

--- src/qt_gui/translations/ru_RU.ts
+++ src/qt_gui/translations/ru_RU.ts
@@ -1166,8 +1166,8 @@
     <message>
       <source>shadPS4 is not found!
 Please change shadPS4 path in settings.</source>
-      <translation type="unfinished">shadPS4 is not found!
-Please change shadPS4 path in settings.</translation>
+      <translation>shadPS4 не найден!
+Пожалуйста, поменяйте путь к shadPS4 в настройках.</translation>
     </message>
   </context>
   <context>
