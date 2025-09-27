edec3a10fabd1bb477a2307d0a685f3239199039
georgemoralis <giorgosmrls@gmail.com>
2025-09-27 11:33:40 +0300

    New translations en_us.ts (Catalan)

--- src/qt_gui/translations/ca_ES.ts
+++ src/qt_gui/translations/ca_ES.ts
@@ -1166,8 +1166,8 @@
     <message>
       <source>shadPS4 is not found!
 Please change shadPS4 path in settings.</source>
-      <translation type="unfinished">shadPS4 is not found!
-Please change shadPS4 path in settings.</translation>
+      <translation>no s'ha trobat ShadPS4!
+Canvia, si us plau, la ruta de ShadPS4 a configuració.</translation>
     </message>
   </context>
   <context>
