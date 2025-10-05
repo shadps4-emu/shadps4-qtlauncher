d119663d1978ae690310770cb829e3add62e1879
rainmakerv2 <30595646+rainmakerv3@users.noreply.github.com>
2025-10-05 16:32:00 +0800

    Add sys modules path selection to GUI

--- src/common/config.h
+++ src/common/config.h
@@ -136,6 +136,8 @@ bool getIsConnectedToNetwork();
 void setConnectedToNetwork(bool enable, bool is_game_specific = false);
 void setUserName(const std::string& name, bool is_game_specific = false);
 void setChooseHomeTab(const std::string& type, bool is_game_specific = false);
+std::filesystem::path getSysModulesPath();
+void setSysModulesPath(const std::filesystem::path& path);
 
 // TODO
 bool GetLoadGameSizeEnabled();
