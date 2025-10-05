6c1f78b7233d5e1a84b67c4192feb6b1ba786c3b
georgemoralis <giorgosmrls@gmail.com>
2025-10-05 15:30:55 +0300

    New translations en_us.ts (Norwegian Bokmal)

--- src/qt_gui/translations/nb_NO.ts
+++ src/qt_gui/translations/nb_NO.ts
@@ -2455,6 +2455,14 @@ Endre mappa til shadPS4 i innstillingene.</translation>
       <source>DLC Path:\nThe folder where game DLC is loaded from.</source>
       <translation>DLC-mappe:\nMappa hvor spill-DLC-ene lastes fra.</translation>
     </message>
+    <message>
+      <source>Additional DMem Allocation</source>
+      <translation type="unfinished">Additional DMem Allocation</translation>
+    </message>
+    <message>
+      <source>Additional DMem Allocation:\nForces allocation of the specified amount of additional DMem. Crashes or causes issues in some games.</source>
+      <translation type="unfinished">Additional DMem Allocation:\nForces allocation of the specified amount of additional DMem. Crashes or causes issues in some games.</translation>
+    </message>
   </context>
   <context>
     <name>TrophyViewer</name>
