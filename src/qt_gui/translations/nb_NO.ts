e8697a766125ca699881611e3e0159506e96531f
georgemoralis <giorgosmrls@gmail.com>
2025-12-10 11:31:25 +0200

    New translations en_us.ts (Norwegian Bokmal)

--- src/qt_gui/translations/nb_NO.ts
+++ src/qt_gui/translations/nb_NO.ts
@@ -621,6 +621,10 @@
       <source>Right Stick Deadzone</source>
       <translation>Høyre analog dødsone</translation>
     </message>
+    <message>
+      <source>Tip: Unmap inputs with right-click</source>
+      <translation type="unfinished">Tip: Unmap inputs with right-click</translation>
+    </message>
   </context>
   <context>
     <name>EditorDialog</name>
@@ -1212,6 +1216,10 @@
       <source>Waiting for inputs</source>
       <translation>Venter på inndata</translation>
     </message>
+    <message>
+      <source>Tip: Unmap inputs with right-click</source>
+      <translation type="unfinished">Tip: Unmap inputs with right-click</translation>
+    </message>
   </context>
   <context>
     <name>IpcClient</name>
@@ -1417,6 +1425,10 @@
     <message>
       <source>Touchpad Right</source>
       <translation>Høyre berøringsplate</translation>
+    </message>
+    <message>
+      <source>Tip: Unmap inputs with right-click</source>
+      <translation type="unfinished">Tip: Unmap inputs with right-click</translation>
     </message>
   </context>
   <context>
@@ -2855,10 +2867,6 @@ Velg en emulatorversjon fra høyre panel.</translation>
       <translation>Fant ingen nedlastings-URL for den valgte ressursen.</translation>
     </message>
     <message>
-      <source>Downloading Pre‑release, please wait...</source>
-      <translation>Laster ned forhåndsutgivelsen, vent litt …</translation>
-    </message>
-    <message>
       <source>Network error while downloading</source>
       <translation>Nettverksfeil ved nedlasting</translation>
     </message>
@@ -2913,6 +2921,10 @@ Velg en emulatorversjon fra høyre panel.</translation>
     <message>
       <source>Unable to get hash of latest pre-release.</source>
       <translation type="unfinished">Unable to get hash of latest pre-release.</translation>
+    </message>
+    <message>
+      <source>Downloading Pre-release (Nightly), please wait...</source>
+      <translation type="unfinished">Downloading Pre-release (Nightly), please wait...</translation>
     </message>
   </context>
   <context>
