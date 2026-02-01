4ba1bfd3e24aa86135e27bfb53b4fc1298cc85f7
georgemoralis <giorgosmrls@gmail.com>
2026-02-01 10:06:07 +0200

    New Crowdin updates (#239)
    * New translations en_us.ts (Japanese)
    
    * New translations en_us.ts (Ukrainian)
    
    * New translations en_us.ts (Ukrainian)
    
    * New translations en_us.ts (Turkish)
    
    * New translations en_us.ts (Turkish)
    
    * New translations en_us.ts (Portuguese, Brazilian)
    
    * New translations en_us.ts (Urdu (Pakistan))
    
    * New translations en_us.ts (Urdu (Pakistan))

--- src/qt_gui/translations/uk_UA.ts
+++ src/qt_gui/translations/uk_UA.ts
@@ -2357,23 +2357,24 @@ Select an emulator version from the right panel.</source>
     </message>
     <message>
       <source>Present Mode:\nConfigures how video output will be presented to your screen.\n\nMailbox: Frames synchronize with your screen&apos;s refresh rate. New frames will replace any pending frames. Reduces latency but may skip frames if running behind.\nFifo: Frames synchronize with your screen&apos;s refresh rate. New frames will be queued behind pending frames. Ensures all frames are presented but may increase latency.\nImmediate: Frames immediately present to your screen when ready. May result in tearing.</source>
-      <translation type="unfinished">Present Mode:\nConfigures how video output will be presented to your screen.\n\nMailbox: Frames synchronize with your screen&apos;s refresh rate. New frames will replace any pending frames. Reduces latency but may skip frames if running behind.\nFifo: Frames synchronize with your screen&apos;s refresh rate. New frames will be queued behind pending frames. Ensures all frames are presented but may increase latency.\nImmediate: Frames immediately present to your screen when ready. May result in tearing.</translation>
+      <translation>Режим показу:\nНалаштовує спосіб виведення відео на ваш екран.\n\nПоштова скринька: Кадри синхронізуються з частотою оновлення вашого екрана. Нові кадри замінять будь-які очікувальні кадри. Зменшує затримку, але може пропускати кадри, якщо відстає.\nFifo: Кадри синхронізуються з частотою оновлення вашого екрана. Нові кадри стають в чергу після очікувальних кадрів. Гарантує показ усіх кадрів, але може збільшити затримку.
+\nНегайно: Кадри негайно зображуються на екрані, коли готові. Може призвести до розривів зображення.</translation>
     </message>
     <message>
       <source>Browse:\nBrowse for a folder to set as the DLC path.</source>
-      <translation type="unfinished">Browse:\nBrowse for a folder to set as the DLC path.</translation>
+      <translation>Перегляд:\nЗнайти теку, яку потрібно встановити як шлях до доповнення.</translation>
     </message>
     <message>
       <source>FSR Settings</source>
-      <translation type="unfinished">FSR Settings</translation>
+      <translation>Налаштування FSR</translation>
     </message>
     <message>
       <source>Enable FSR</source>
-      <translation type="unfinished">Enable FSR</translation>
+      <translation>Увімкнути FSR</translation>
     </message>
     <message>
       <source>Enable RCAS (sharpening)</source>
-      <translation type="unfinished">Enable RCAS (sharpening)</translation>
+      <translation>Увімкнути RCAS (гострота)</translation>
     </message>
     <message>
       <source>RCAS Attenuation:</source>
@@ -2405,87 +2406,87 @@ Select an emulator version from the right panel.</source>
     </message>
     <message>
       <source>Enable Motion Controls:\nWhen enabled it will use the controller&apos;s motion control if supported.</source>
-      <translation type="unfinished">Enable Motion Controls:\nWhen enabled it will use the controller&apos;s motion control if supported.</translation>
+      <translation>Увімкнути керування рухом:\nЯкщо ввімкнено, буде використовуватися керування рухом контролера, якщо це підтримується.</translation>
     </message>
     <message>
       <source>Vblank Frequency</source>
-      <translation type="unfinished">Vblank Frequency</translation>
+      <translation>Частота Vblank</translation>
     </message>
     <message>
       <source>Vblank Frequency:\nThe frame rate at which the emulator refreshes at (60hz is the baseline, whether the game runs at 30 or 60fps). Changing this may have adverse effects, such as increasing the game speed, or breaking critical game functionality that does not expect this to change!</source>
-      <translation type="unfinished">Vblank Frequency:\nThe frame rate at which the emulator refreshes at (60hz is the baseline, whether the game runs at 30 or 60fps). Changing this may have adverse effects, such as increasing the game speed, or breaking critical game functionality that does not expect this to change!</translation>
+      <translation>Частота Vblank:\nЧастота кадрів, з якою працює емулятор (базова частота — 60 Гц, незалежно від того, чи гра працює у 30 чи 60 к / с). Зміна цього параметра може призвести до негативних наслідків, таких як прискорення гри або порушення критичної функціональності, яка не розрахована на такі зміни!</translation>
     </message>
     <message>
       <source>Enable Direct Memory Access</source>
-      <translation type="unfinished">Enable Direct Memory Access</translation>
+      <translation>Увімкнути прямий доступ до пам’яті</translation>
     </message>
     <message>
       <source>Enable Devkit Console Mode</source>
-      <translation type="unfinished">Enable Devkit Console Mode</translation>
+      <translation>Увімкнути режим консолі для розробників</translation>
     </message>
     <message>
       <source>Set &quot;PSN signed-in&quot; to True</source>
-      <translation type="unfinished">Set &quot;PSN signed-in&quot; to True</translation>
+      <translation>Використовувати емуляцію доступу до PSN</translation>
     </message>
     <message>
       <source>Set &quot;Network Connected&quot; to True</source>
-      <translation type="unfinished">Set &quot;Network Connected&quot; to True</translation>
+      <translation>Використовувати емуляцію доступу до мережі</translation>
     </message>
     <message>
       <source>Miscellaneous</source>
-      <translation type="unfinished">Miscellaneous</translation>
+      <translation>Різне</translation>
     </message>
     <message>
       <source>Frontend</source>
-      <translation type="unfinished">Frontend</translation>
+      <translation>Інтерфейс</translation>
     </message>
     <message>
       <source>Experimental</source>
-      <translation type="unfinished">Experimental</translation>
+      <translation>Експериментальні</translation>
     </message>
     <message>
       <source>Show Splash Screen When Launching Game</source>
-      <translation type="unfinished">Show Splash Screen When Launching Game</translation>
+      <translation>Показувати заставку при запуску гри</translation>
     </message>
     <message>
       <source>General Frontend Settings</source>
-      <translation type="unfinished">General Frontend Settings</translation>
+      <translation>Загальні налаштування інтерфейсу</translation>
     </message>
     <message>
       <source>Enable PS4 Neo Mode</source>
-      <translation type="unfinished">Enable PS4 Neo Mode</translation>
+      <translation>Увімкнути режим PS4 Neo</translation>
     </message>
     <message>
       <source>Enable Direct Memory Access:\nEnables arbitrary memory access from the GPU to CPU memory.</source>
-      <translation type="unfinished">Enable Direct Memory Access:\nEnables arbitrary memory access from the GPU to CPU memory.</translation>
+      <translation>Увімкнути прямий доступ до пам’яті:\nНадає відеокарті довільний доступ до пам’яті центрального процесора.</translation>
     </message>
     <message>
       <source>Enable PS4 Neo Mode:\nAdds support for PS4 Pro emulation and memory size. Currently causes instability in a large number of tested games.</source>
-      <translation type="unfinished">Enable PS4 Neo Mode:\nAdds support for PS4 Pro emulation and memory size. Currently causes instability in a large number of tested games.</translation>
+      <translation>Увімкнути режим PS4 Neo:\nДодає підтримку емуляції PS4 Pro та розміру пам’яті. Наразі викликає нестабільність у великій кількості протестованих ігор.</translation>
     </message>
     <message>
       <source>Enable Devkit Console Mode:\nAdds support for Devkit console memory size.</source>
-      <translation type="unfinished">Enable Devkit Console Mode:\nAdds support for Devkit console memory size.</translation>
+      <translation>Увімкнути режим консолі для розробників:\nДодає підтримку розміру пам’яті консолі для розробників.</translation>
     </message>
     <message>
       <source>Set Network Connected to True:\nForces games to detect an active network connection. Actual online capabilities are not yet supported.</source>
-      <translation type="unfinished">Set Network Connected to True:\nForces games to detect an active network connection. Actual online capabilities are not yet supported.</translation>
+      <translation>Використовувати емуляцію доступу до мережі:\nЗмушує ігри визначати активне мережеве з’єднання. Фактичні онлайн-можливості ще не підтримуються.</translation>
     </message>
     <message>
       <source>Set PSN Signed-in to True:\nForces games to detect an active PSN sign-in. Actual PSN capabilities are not supported.</source>
-      <translation type="unfinished">Set PSN Signed-in to True:\nForces games to detect an active PSN sign-in. Actual PSN capabilities are not supported.</translation>
+      <translation>Використовувати емуляцію доступу до PSN:\nЗмушує ігри виявляти активний вхід у PSN. Фактичні можливості PSN не підтримуються.</translation>
     </message>
     <message>
       <source>Enable Vulkan Validation Layers:\nEnables a system that validates the state of the Vulkan renderer and logs information about its internal state.\nThis will reduce performance and likely change the behavior of emulation.\nYou need the Vulkan SDK for this to work.</source>
-      <translation type="unfinished">Enable Vulkan Validation Layers:\nEnables a system that validates the state of the Vulkan renderer and logs information about its internal state.\nThis will reduce performance and likely change the behavior of emulation.\nYou need the Vulkan SDK for this to work.</translation>
+      <translation>Увімкнути шари перевірки Vulkan:\nВмикає систему, яка перевіряє стан рендера Vulkan та записує інформацію про його внутрішній стан.\nЦе знизить продуктивність і, ймовірно, змінить поведінку емуляції.\nДля роботи цієї функції потрібен Vulkan SDK.</translation>
     </message>
     <message>
       <source>Audio Device (general)</source>
-      <translation type="unfinished">Audio Device (general)</translation>
+      <translation>Аудіопристрій (загальний)</translation>
     </message>
     <message>
       <source>Audio Device (DS4 speaker)</source>
-      <translation type="unfinished">Audio Device (DS4 speaker)</translation>
+      <translation>Аудіопристрій (динамік DS4)</translation>
     </message>
     <message>
       <source>Enable Shaders Dumping:\nFor the sake of technical debugging, saves the game&apos;s shaders to a folder as they render.</source>
