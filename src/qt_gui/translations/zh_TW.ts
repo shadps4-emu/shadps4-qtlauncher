172bb251602d822392ebdc8afd459b795deb3a0b
georgemoralis <giorgosmrls@gmail.com>
2026-03-10 07:36:53 +0200

    New translations en_us.ts (Chinese Traditional)

--- src/qt_gui/translations/zh_TW.ts
+++ src/qt_gui/translations/zh_TW.ts
@@ -2618,16 +2618,16 @@ Select an emulator version from the right panel.</source>
       <translation type="unfinished">Disabled</translation>
     </message>
     <message>
-      <source>Low</source>
-      <translation type="unfinished">Low</translation>
-    </message>
-    <message>
-      <source>High</source>
-      <translation type="unfinished">High</translation>
-    </message>
-    <message>
       <source>Readbacks:\nEnable GPU memory readbacks and writebacks.\nThis is required for proper behavior in some games.\nMight cause stability and/or performance issues.</source>
       <translation type="unfinished">Readbacks:\nEnable GPU memory readbacks and writebacks.\nThis is required for proper behavior in some games.\nMight cause stability and/or performance issues.</translation>
+    </message>
+    <message>
+      <source>Relaxed</source>
+      <translation type="unfinished">Relaxed</translation>
+    </message>
+    <message>
+      <source>Precise</source>
+      <translation type="unfinished">Precise</translation>
     </message>
   </context>
   <context>
