024bc289b118a87eff2e233a635236ccb2d6175b
georgemoralis <giorgosmrls@gmail.com>
2026-04-05 07:53:25 +0300

    New translations en_us.ts (Japanese)

--- src/qt_gui/translations/ja_JP.ts
+++ src/qt_gui/translations/ja_JP.ts
@@ -2073,10 +2073,6 @@ Select an emulator version from the right panel.</translation>
       <translation>カーソルを隠すまでの非アクティブ期間</translation>
     </message>
     <message>
-      <source>Microphone</source>
-      <translation>マイク</translation>
-    </message>
-    <message>
       <source>None</source>
       <translation>なし</translation>
     </message>
@@ -2835,6 +2831,26 @@ Select an emulator version from the right panel.</translation>
     <message>
       <source>Browse:\nBrowse for a folder to set as the home folder.</source>
       <translation type="unfinished">Browse:\nBrowse for a folder to set as the home folder.</translation>
+    </message>
+    <message>
+      <source>Audio</source>
+      <translation type="unfinished">Audio</translation>
+    </message>
+    <message>
+      <source>Audio Backend</source>
+      <translation type="unfinished">Audio Backend</translation>
+    </message>
+    <message>
+      <source>Mic Device</source>
+      <translation type="unfinished">Mic Device</translation>
+    </message>
+    <message>
+      <source>Devices</source>
+      <translation type="unfinished">Devices</translation>
+    </message>
+    <message>
+      <source>Camera</source>
+      <translation type="unfinished">Camera</translation>
     </message>
   </context>
   <context>
