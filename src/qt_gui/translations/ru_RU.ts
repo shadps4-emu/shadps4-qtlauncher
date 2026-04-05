8a41518a9e28c01221ce442da499a33594263bd3
georgemoralis <giorgosmrls@gmail.com>
2026-04-05 07:53:10 +0300

    New translations en_us.ts (Russian)

--- src/qt_gui/translations/ru_RU.ts
+++ src/qt_gui/translations/ru_RU.ts
@@ -2073,10 +2073,6 @@ Select an emulator version from the right panel.</source>
       <translation>Время скрытия курсора при бездействии</translation>
     </message>
     <message>
-      <source>Microphone</source>
-      <translation>Микрофон</translation>
-    </message>
-    <message>
       <source>None</source>
       <translation>Нет</translation>
     </message>
@@ -2835,6 +2831,26 @@ Select an emulator version from the right panel.</source>
     <message>
       <source>Browse:\nBrowse for a folder to set as the home folder.</source>
       <translation>Просмотр:\nВыберите папку, которую хотите установить в качестве домашней папки.</translation>
+    </message>
+    <message>
+      <source>Audio</source>
+      <translation type="unfinished">Audio</translation>
+    </message>
+    <message>
+      <source>Audio Backend</source>
+      <translation type="unfinished">Audio Backend</translation>
+    </message>
+    <message>
+      <source>Mic Device</source>
+      <translation type="unfinished">Mic Device</translation>
+    </message>
+    <message>
+      <source>Devices</source>
+      <translation type="unfinished">Devices</translation>
+    </message>
+    <message>
+      <source>Camera</source>
+      <translation type="unfinished">Camera</translation>
     </message>
   </context>
   <context>
