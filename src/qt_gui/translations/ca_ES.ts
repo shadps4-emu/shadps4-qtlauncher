9aafbb523999d6231611346f7eb72e365f00677b
georgemoralis <giorgosmrls@gmail.com>
2026-04-05 07:53:15 +0300

    New translations en_us.ts (Catalan)

--- src/qt_gui/translations/ca_ES.ts
+++ src/qt_gui/translations/ca_ES.ts
@@ -2073,10 +2073,6 @@ Selecciona una versió del panell dret.</translation>
       <translation>Temps d'espera per ocultar el ratolí</translation>
     </message>
     <message>
-      <source>Microphone</source>
-      <translation>Micròfon</translation>
-    </message>
-    <message>
       <source>None</source>
       <translation>Cap</translation>
     </message>
@@ -2835,6 +2831,26 @@ Selecciona una versió del panell dret.</translation>
     <message>
       <source>Browse:\nBrowse for a folder to set as the home folder.</source>
       <translation>Navegador:\nNavega per una carpeta per escollir-la com a carpeta home.</translation>
+    </message>
+    <message>
+      <source>Audio</source>
+      <translation type="unfinished">Audio</translation>
+    </message>
+    <message>
+      <source>Audio Backend</source>
+      <translation type="unfinished">Audio Backend</translation>
+    </message>
+    <message>
+      <source>Mic Device</source>
+      <translation type="unfinished">Mic Device</translation>
+    </message>
+    <message>
+      <source>Devices</source>
+      <translation type="unfinished">Devices</translation>
+    </message>
+    <message>
+      <source>Camera</source>
+      <translation type="unfinished">Camera</translation>
     </message>
   </context>
   <context>
