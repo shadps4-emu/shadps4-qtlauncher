edc51140e36efe8d34a0079edb31cd9cd2877e10
georgemoralis <giorgosmrls@gmail.com>
2026-04-11 11:12:00 +0300

    New translations en_us.ts (Arabic)

--- src/qt_gui/translations/ar_SA.ts
+++ src/qt_gui/translations/ar_SA.ts
@@ -1292,24 +1292,12 @@ Please enter your keys and save them.</translation>
       <translation type="unfinished">Shader Cache</translation>
     </message>
     <message>
-      <source>User 1</source>
-      <translation>المستخدم 1</translation>
-    </message>
-    <message>
-      <source>User 2</source>
-      <translation>المستخدم 2</translation>
-    </message>
-    <message>
-      <source>User 3</source>
-      <translation>المستخدم 3</translation>
-    </message>
-    <message>
-      <source>User 4</source>
-      <translation>المستخدم 4</translation>
-    </message>
-    <message>
       <source>This game has no save folder to open!</source>
       <translation type="unfinished">This game has no save folder to open!</translation>
+    </message>
+    <message>
+      <source>User</source>
+      <translation type="unfinished">User</translation>
     </message>
   </context>
   <context>
@@ -2898,6 +2886,14 @@ Select an emulator version from the right panel.</source>
     <message>
       <source>Show Hidden Trophies</source>
       <translation>عرض الكؤوس المخفية</translation>
+    </message>
+    <message>
+      <source>Select User:</source>
+      <translation type="unfinished">Select User:</translation>
+    </message>
+    <message>
+      <source>%1 trophies for %2</source>
+      <translation type="unfinished">%1 trophies for %2</translation>
     </message>
   </context>
   <context>
