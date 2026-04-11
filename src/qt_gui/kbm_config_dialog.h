9615ab74a7b366ae10dd69a1763884f996b0bf89
rainmakerv2 <30595646+rainmakerv3@users.noreply.github.com>
2026-04-12 01:45:12 +0800

    clang

--- src/qt_gui/kbm_config_dialog.h
+++ src/qt_gui/kbm_config_dialog.h
@@ -9,33 +9,35 @@
 #include "string"
 
 class EditorDialog : public QDialog {
-Q_OBJECT // Necessary for using Qt's meta-object system (signals/slots)
-    public : explicit EditorDialog(QWidget* parent = nullptr); // Constructor
+    Q_OBJECT // Necessary for using Qt's meta-object system (signals/slots)
 
-    protected:
-        void closeEvent(QCloseEvent* event) override; // Override close event
-        void keyPressEvent(QKeyEvent* event) override;
+public:
+    explicit EditorDialog(QWidget* parent = nullptr); // Constructor
 
-    private:
-        QPlainTextEdit* editor; // Editor widget for the config file
-        QFont editorFont;       // To handle the text size
-        QString originalConfig; // Starting config string
-        std::string gameId;
+protected:
+    void closeEvent(QCloseEvent* event) override; // Override close event
+    void keyPressEvent(QKeyEvent* event) override;
 
-        QComboBox* gameComboBox; // Combo box for selecting game configurations
+private:
+    QPlainTextEdit* editor; // Editor widget for the config file
+    QFont editorFont;       // To handle the text size
+    QString originalConfig; // Starting config string
+    std::string gameId;
 
-        void loadFile(QString game); // Function to load the config file
-        void saveFile(QString game); // Function to save the config file
-        void loadInstalledGames();   // Helper to populate gameComboBox
-        bool hasUnsavedChanges();    // Checks for unsaved changes
+    QComboBox* gameComboBox; // Combo box for selecting game configurations
 
-    private slots:
-        void onSaveClicked();   // Save button slot
-        void onCancelClicked(); // Slot for handling cancel button
-        void onHelpClicked();   // Slot for handling help button
-        void onResetToDefaultClicked();
-        void onGameSelectionChanged(const QString& game); // Slot for game selection changes
+    void loadFile(QString game); // Function to load the config file
+    void saveFile(QString game); // Function to save the config file
+    void loadInstalledGames();   // Helper to populate gameComboBox
+    bool hasUnsavedChanges();    // Checks for unsaved changes
 
-    signals:
-        void configSaved();
+private slots:
+    void onSaveClicked();   // Save button slot
+    void onCancelClicked(); // Slot for handling cancel button
+    void onHelpClicked();   // Slot for handling help button
+    void onResetToDefaultClicked();
+    void onGameSelectionChanged(const QString& game); // Slot for game selection changes
+
+signals:
+    void configSaved();
 };
