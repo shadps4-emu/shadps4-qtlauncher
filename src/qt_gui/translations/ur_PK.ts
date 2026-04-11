3bcda9bd4537c69c787495698a31c2ab4d8fbfe9
georgemoralis <giorgosmrls@gmail.com>
2026-04-11 11:12:20 +0300

    New translations en_us.ts (Urdu (Pakistan))

--- src/qt_gui/translations/ur_PK.ts
+++ src/qt_gui/translations/ur_PK.ts
@@ -1294,24 +1294,12 @@ Please enter your keys and save them.</translation>
       <translation type="unfinished">Shader Cache</translation>
     </message>
     <message>
-      <source>User 1</source>
-      <translation type="unfinished">User 1</translation>
-    </message>
-    <message>
-      <source>User 2</source>
-      <translation type="unfinished">User 2</translation>
-    </message>
-    <message>
-      <source>User 3</source>
-      <translation type="unfinished">User 3</translation>
-    </message>
-    <message>
-      <source>User 4</source>
-      <translation type="unfinished">User 4</translation>
-    </message>
-    <message>
       <source>This game has no save folder to open!</source>
       <translation type="unfinished">This game has no save folder to open!</translation>
+    </message>
+    <message>
+      <source>User</source>
+      <translation type="unfinished">User</translation>
     </message>
   </context>
   <context>
@@ -2900,6 +2888,14 @@ Select an emulator version from the right panel.</translation>
     <message>
       <source>Show Hidden Trophies</source>
       <translation type="unfinished">Show Hidden Trophies</translation>
+    </message>
+    <message>
+      <source>Select User:</source>
+      <translation type="unfinished">Select User:</translation>
+    </message>
+    <message>
+      <source>%1 trophies for %2</source>
+      <translation type="unfinished">%1 trophies for %2</translation>
     </message>
   </context>
   <context>
