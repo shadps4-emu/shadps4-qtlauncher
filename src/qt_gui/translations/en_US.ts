34f585cf0089725e64915a6a781514c46735b14c
georgemoralis <4313123+georgemoralis@users.noreply.github.com>
2026-04-20 02:36:16 +0000

    [ci skip] Qt GUI: Update Translation.

--- src/qt_gui/translations/en_US.ts
+++ src/qt_gui/translations/en_US.ts
@@ -2240,14 +2240,6 @@ Select an emulator version from the right panel.</source>
         <translation>Enable Discord Rich Presence:\nDisplays the emulator icon and relevant information on your Discord profile.</translation>
     </message>
     <message>
-        <source>Log Type:\nSets whether to synchronize the output of the log window for performance. May have adverse effects on emulation.</source>
-        <translation>Log Type:\nSets whether to synchronize the output of the log window for performance. May have adverse effects on emulation.</translation>
-    </message>
-    <message>
-        <source>Log Filter:\nFilters the log to only print specific information.\nExamples: &quot;Core:Trace&quot; &quot;Lib.Pad:Debug Common.Filesystem:Error&quot; &quot;*:Critical&quot;\nLevels: Trace, Debug, Info, Warning, Error, Critical - in this order, a specific level silences all levels preceding it in the list and logs every level after it.</source>
-        <translation>Log Filter:\nFilters the log to only print specific information.\nExamples: &quot;Core:Trace&quot; &quot;Lib.Pad:Debug Common.Filesystem:Error&quot; &quot;*:Critical&quot;\nLevels: Trace, Debug, Info, Warning, Error, Critical - in this order, a specific level silences all levels preceding it in the list and logs every level after it.</translation>
-    </message>
-    <message>
         <source>Background Image:\nControl the opacity of the game background image.</source>
         <translation>Background Image:\nControl the opacity of the game background image.</translation>
     </message>
@@ -2354,14 +2346,6 @@ Select an emulator version from the right panel.</source>
     <message>
         <source>Browse</source>
         <translation type="unfinished">Browse</translation>
-    </message>
-    <message>
-        <source>async</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>sync</source>
-        <translation type="unfinished"></translation>
     </message>
     <message>
         <source>Auto Select</source>
@@ -2825,6 +2809,70 @@ Select an emulator version from the right panel.</source>
     </message>
     <message>
         <source>Camera</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Log Sync</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Log Skip Duplicate</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Log Max Skip Duration</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source> ms</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Log Size Limit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source> B</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Log Append</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>wincolor</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>msvc</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Log Append:\nAppend to existing logs.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Log Filter:\nFilters the log to only print specific information.\nExamples: &quot;Core=trace&quot; &quot;Lib.Pad=debug,Common.Filesystem=error&quot; &quot;critical&quot;\nLevels: trace, debug, info, warning, error, critical - in this order, a specific level silences all levels preceding it in the list and logs every level after it.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Log Max Skip Duration:\nInterval without writing same lines (ms) - only if &apos;Log Skip Duplicate&apos; enabled.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Log Size Limit:\nMaximum size of log files (bytes).</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Log Skip Duplicate:\nSave storage by avoiding writing log that is identical.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Log Sync:\nSwitch between sync (order) or async (performance).</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Log Type:\nChoose between wincolor or msvc log types.\nwincolor: Default logging for Windows\nmsvc: Logging for debugging</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
