8fa1444a560fbdc813d9d842d5a84451b2c63036
rainmakerv2 <30595646+rainmakerv3@users.noreply.github.com>
2026-05-08 18:02:17 +0800

    add new hotkeys

--- src/qt_gui/hotkeys.cpp
+++ src/qt_gui/hotkeys.cpp
@@ -49,14 +49,23 @@ Hotkeys::Hotkeys(std::shared_ptr<IpcClient> ipc_client, bool isGameRunning, QWid
     CheckGamePad();
     installEventFilter(this);
 
-    PadButtonsList = {ui->fpsButtonPad,    ui->quitButtonPad,   ui->fullscreenButtonPad,
-                      ui->pauseButtonPad,  ui->reloadButtonPad, ui->volUpButtonPad,
-                      ui->volDownButtonPad};
+    PadButtonsList = {ui->fpsButtonPad,     ui->quitButtonPad,       ui->fullscreenButtonPad,
+                      ui->pauseButtonPad,   ui->reloadButtonPad,     ui->volUpButtonPad,
+                      ui->volDownButtonPad, ui->screenshotButtonPad, ui->screenshotOverlayButtonPad,
+                      ui->settingsButtonPad};
 
-    KBButtonsList = {ui->fpsButtonKB,         ui->quitButtonKB,    ui->fullscreenButtonKB,
-                     ui->pauseButtonKB,       ui->reloadButtonKB,  ui->renderdocButton,
-                     ui->mouseJoystickButton, ui->mouseGyroButton, ui->volUpButtonKB,
-                     ui->volDownButtonKB};
+    KBButtonsList = {ui->fpsButtonKB,
+                     ui->quitButtonKB,
+                     ui->fullscreenButtonKB,
+                     ui->pauseButtonKB,
+                     ui->reloadButtonKB,
+                     ui->mouseJoystickButton,
+                     ui->mouseGyroButton,
+                     ui->volUpButtonKB,
+                     ui->volDownButtonKB,
+                     ui->screenshotButtonKB,
+                     ui->screenshotOverlayButtonKB,
+                     ui->settingsButtonKB};
 
     connect(ui->buttonBox, &QDialogButtonBox::clicked, this, [this](QAbstractButton* button) {
         if (button == ui->buttonBox->button(QDialogButtonBox::Save)) {
@@ -134,6 +143,9 @@ void Hotkeys::SetDefault() {
     ui->volUpButtonPad->setText("unmapped");
     ui->volDownButtonPad->setText("unmapped");
     ui->reloadButtonPad->setText("unmapped");
+    ui->screenshotButtonPad->setText("unmapped");
+    ui->screenshotOverlayButtonPad->setText("unmapped");
+    ui->settingsButtonPad->setText("unmapped");
 
     ui->fpsButtonKB->setText("f10");
     ui->quitButtonKB->setText("lctrl, lshift, end");
@@ -142,8 +154,10 @@ void Hotkeys::SetDefault() {
     ui->volUpButtonKB->setText("kpplus");
     ui->volDownButtonKB->setText("kpminus");
     ui->reloadButtonKB->setText("f8");
+    ui->screenshotButtonKB->setText("f12");
+    ui->screenshotOverlayButtonKB->setText("lalt, f12");
+    ui->settingsButtonKB->setText("f3");
 
-    ui->renderdocButton->setText("f12");
     ui->mouseJoystickButton->setText("f7");
     ui->mouseGyroButton->setText("f6");
 }
@@ -182,13 +196,22 @@ void Hotkeys::SaveHotkeys(bool CloseOnSave) {
     add_mapping(ui->reloadButtonKB->text(), "hotkey_reload_inputs");
     lines.push_back("");
 
-    add_mapping(ui->volUpButtonPad->text(), "hotkey_volume_up ");
-    add_mapping(ui->volUpButtonKB->text(), "hotkey_volume_up ");
-    add_mapping(ui->volDownButtonPad->text(), "hotkey_volume_down ");
-    add_mapping(ui->volDownButtonKB->text(), "hotkey_volume_down ");
+    add_mapping(ui->volUpButtonPad->text(), "hotkey_volume_up");
+    add_mapping(ui->volUpButtonKB->text(), "hotkey_volume_up");
+    add_mapping(ui->volDownButtonPad->text(), "hotkey_volume_down");
+    add_mapping(ui->volDownButtonKB->text(), "hotkey_volume_down");
     lines.push_back("");
 
-    add_mapping(ui->renderdocButton->text(), "hotkey_renderdoc_capture");
+    add_mapping(ui->screenshotButtonPad->text(), "hotkey_capture_frame");
+    add_mapping(ui->screenshotButtonKB->text(), "hotkey_capture_frame");
+    add_mapping(ui->screenshotOverlayButtonPad->text(), "hotkey_screenshot_with_overlays");
+    add_mapping(ui->screenshotOverlayButtonKB->text(), "hotkey_screenshot_with_overlays");
+    lines.push_back("");
+
+    add_mapping(ui->settingsButtonPad->text(), "hotkey_emulator_settings");
+    add_mapping(ui->settingsButtonKB->text(), "hotkey_emulator_settings");
+    lines.push_back("");
+
     add_mapping(ui->mouseJoystickButton->text(), "hotkey_toggle_mouse_to_joystick");
     add_mapping(ui->mouseGyroButton->text(), "hotkey_toggle_mouse_to_gyro");
 
@@ -319,8 +342,6 @@ void Hotkeys::LoadHotkeys() {
             controllerInputDetected
                 ? ui->reloadButtonPad->setText(QString::fromStdString(input_string))
                 : ui->reloadButtonKB->setText(QString::fromStdString(input_string));
-        } else if (output_string.contains("hotkey_renderdoc_capture")) {
-            ui->renderdocButton->setText(QString::fromStdString(input_string));
         } else if (output_string.contains("hotkey_toggle_mouse_to_joystick")) {
             ui->mouseJoystickButton->setText(QString::fromStdString(input_string));
         } else if (output_string.contains("hotkey_toggle_mouse_to_gyro")) {
@@ -333,6 +354,18 @@ void Hotkeys::LoadHotkeys() {
             controllerInputDetected
                 ? ui->volDownButtonPad->setText(QString::fromStdString(input_string))
                 : ui->volDownButtonKB->setText(QString::fromStdString(input_string));
+        } else if (output_string.contains("hotkey_capture_frame")) {
+            controllerInputDetected
+                ? ui->screenshotButtonPad->setText(QString::fromStdString(input_string))
+                : ui->screenshotButtonKB->setText(QString::fromStdString(input_string));
+        } else if (output_string.contains("hotkey_screenshot_with_overlays")) {
+            controllerInputDetected
+                ? ui->screenshotOverlayButtonPad->setText(QString::fromStdString(input_string))
+                : ui->screenshotOverlayButtonKB->setText(QString::fromStdString(input_string));
+        } else if (output_string.contains("hotkey_emulator_settings")) {
+            controllerInputDetected
+                ? ui->settingsButtonPad->setText(QString::fromStdString(input_string))
+                : ui->settingsButtonKB->setText(QString::fromStdString(input_string));
         }
     }
 
