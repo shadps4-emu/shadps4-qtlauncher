938e127a50058d6449fc88e046975b4868d52677
georgemoralis <giorgosmrls@gmail.com>
2026-05-20 12:37:07 +0300

    New translations en_us.ts (Polish)
    [ci skip]

--- src/qt_gui/translations/pl_PL.ts
+++ src/qt_gui/translations/pl_PL.ts
@@ -1448,40 +1448,12 @@ Please enter your keys and save them.</translation>
       <translation>Skonfiguruj sterowanie</translation>
     </message>
     <message>
-      <source>D-Pad</source>
-      <translation>Krzyżak</translation>
-    </message>
-    <message>
-      <source>Up</source>
-      <translation>Strzałka w górę</translation>
-    </message>
-    <message>
       <source>unmapped</source>
       <translation>nieprzypisane</translation>
     </message>
     <message>
-      <source>Left</source>
-      <translation>Strzałka w lewo</translation>
-    </message>
-    <message>
-      <source>Right</source>
-      <translation>Strzałka w prawo</translation>
-    </message>
-    <message>
-      <source>Down</source>
-      <translation>Strzałka w dół</translation>
-    </message>
-    <message>
       <source>Left Analog Halfmode</source>
       <translation>Połowiczny tryb lewego drążka</translation>
-    </message>
-    <message>
-      <source>hold to move left stick at half-speed</source>
-      <translation>przytrzymaj, aby przesuwać lewy drążek dwa razy wolniej</translation>
-    </message>
-    <message>
-      <source>Left Stick</source>
-      <translation>Lewy drążek</translation>
     </message>
     <message>
       <source>Config Selection</source>
@@ -1524,10 +1496,6 @@ Please enter your keys and save them.</translation>
       <translation>uwaga: kliknij przycisk Pomoc/Specjalne skróty klawiszowe, aby uzyskać więcej informacji</translation>
     </message>
     <message>
-      <source>Face Buttons</source>
-      <translation>Przednie przyciski</translation>
-    </message>
-    <message>
       <source>Triangle</source>
       <translation>Trójkąt</translation>
     </message>
@@ -1548,14 +1516,6 @@ Please enter your keys and save them.</translation>
       <translation>Połowiczny tryb prawego drążka</translation>
     </message>
     <message>
-      <source>hold to move right stick at half-speed</source>
-      <translation>przytrzymaj, aby przesuwać prawy drążek dwa razy wolniej</translation>
-    </message>
-    <message>
-      <source>Right Stick</source>
-      <translation>Prawy drążek</translation>
-    </message>
-    <message>
       <source>Speed Offset (def 0.125):</source>
       <translation>Offset prędkości (def 0,125):</translation>
     </message>
@@ -1570,14 +1530,6 @@ Please enter your keys and save them.</translation>
     <message>
       <source>Speed Multiplier (def 1.0):</source>
       <translation>Mnożnik prędkości (def1.0):</translation>
-    </message>
-    <message>
-      <source>Common Config Selected</source>
-      <translation>Wybrano wspólną konfigurację</translation>
-    </message>
-    <message>
-      <source>This button copies mappings from the Common Config to the currently selected profile, and cannot be used when the currently selected profile is the Common Config.</source>
-      <translation>Przycisk ten kopiuje mapowanie ze wspólnej konfiguracji do aktualnie wybranego profilu. Nie może być użyty, gdy aktualnie wybranym profilem jest wspólna konfiguracja.</translation>
     </message>
     <message>
       <source>Copy values from Common Config</source>
@@ -1642,6 +1594,94 @@ Please enter your keys and save them.</translation>
     <message>
       <source>Tip: Unmap inputs with right-click</source>
       <translation type="unfinished">Tip: Unmap inputs with right-click</translation>
+    </message>
+    <message>
+      <source>Output</source>
+      <translation type="unfinished">Output</translation>
+    </message>
+    <message>
+      <source>Input</source>
+      <translation type="unfinished">Input</translation>
+    </message>
+    <message>
+      <source>Alternate Input 1</source>
+      <translation type="unfinished">Alternate Input 1</translation>
+    </message>
+    <message>
+      <source>Alternate Input 2</source>
+      <translation type="unfinished">Alternate Input 2</translation>
+    </message>
+    <message>
+      <source>Dpad Up</source>
+      <translation type="unfinished">Dpad Up</translation>
+    </message>
+    <message>
+      <source>Dpad Down</source>
+      <translation type="unfinished">Dpad Down</translation>
+    </message>
+    <message>
+      <source>Dpad Left</source>
+      <translation type="unfinished">Dpad Left</translation>
+    </message>
+    <message>
+      <source>Dpad Right</source>
+      <translation type="unfinished">Dpad Right</translation>
+    </message>
+    <message>
+      <source>Left Stick Up</source>
+      <translation type="unfinished">Left Stick Up</translation>
+    </message>
+    <message>
+      <source>Left Stick Down</source>
+      <translation type="unfinished">Left Stick Down</translation>
+    </message>
+    <message>
+      <source>Left Stick Left</source>
+      <translation type="unfinished">Left Stick Left</translation>
+    </message>
+    <message>
+      <source>Left Stick Right</source>
+      <translation type="unfinished">Left Stick Right</translation>
+    </message>
+    <message>
+      <source>Right Stick Up</source>
+      <translation type="unfinished">Right Stick Up</translation>
+    </message>
+    <message>
+      <source>Right Stick Down</source>
+      <translation type="unfinished">Right Stick Down</translation>
+    </message>
+    <message>
+      <source>Right Stick Left</source>
+      <translation type="unfinished">Right Stick Left</translation>
+    </message>
+    <message>
+      <source>Right Stick Right</source>
+      <translation type="unfinished">Right Stick Right</translation>
+    </message>
+    <message>
+      <source>L1</source>
+      <translation type="unfinished">L1</translation>
+    </message>
+    <message>
+      <source>R1</source>
+      <translation type="unfinished">R1</translation>
+    </message>
+    <message>
+      <source>L2</source>
+      <translation type="unfinished">L2</translation>
+    </message>
+    <message>
+      <source>R2</source>
+      <translation type="unfinished">R2</translation>
+    </message>
+    <message>
+      <source>L3</source>
+      <translation type="unfinished">L3</translation>
+    </message>
+    <message>
+      <source>R3</source>
+      <translation type="unfinished">R3</translation>
     </message>
   </context>
   <context>
