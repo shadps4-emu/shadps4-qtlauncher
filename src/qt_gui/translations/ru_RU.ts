6f11d11c4db400f0d36d7d2a2062fca1e88b1299
georgemoralis <giorgosmrls@gmail.com>
2026-07-06 06:12:30 +0300

    New translations en_us.ts (Russian)
    [ci skip]

--- src/qt_gui/translations/ru_RU.ts
+++ src/qt_gui/translations/ru_RU.ts
@@ -2993,7 +2993,7 @@ Select an emulator version from the right panel.</source>
     </message>
     <message>
       <source>shadNet:\nCompatibility is very limited at the moment.\nYou can register at https://www.shadps4.net/shadnet/register/.</source>
-      <translation type="unfinished">shadNet:\nCompatibility is very limited at the moment.\nYou can register at https://www.shadps4.net/shadnet/register/.</translation>
+      <translation>shadNet:\nСовместимость в данный момент очень ограничена.\nВы можете зарегистрироваться на https://www.shadps4.net/shadnet/register/.</translation>
     </message>
   </context>
   <context>
