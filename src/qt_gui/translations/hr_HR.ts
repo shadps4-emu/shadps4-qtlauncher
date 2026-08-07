3cc7ae69247d85ec6aafab934e885677f8c05b1e
georgemoralis <giorgosmrls@gmail.com>
2026-08-07 09:01:57 +0300

    New translations en_us.ts (Croatian)
    [ci skip]

--- src/qt_gui/translations/hr_HR.ts
+++ src/qt_gui/translations/hr_HR.ts
@@ -3002,6 +3002,14 @@ Izaziva rušenje ili probleme u nekim igrama.</translation>
       <source>shadNet:\nCompatibility is very limited at the moment.\nYou can register at https://www.shadps4.net/shadnet/register/.</source>
       <translation type="unfinished">shadNet:\nCompatibility is very limited at the moment.\nYou can register at https://www.shadps4.net/shadnet/register/.</translation>
     </message>
+    <message>
+      <source>Red Zone Protection</source>
+      <translation type="unfinished">Red Zone Protection</translation>
+    </message>
+    <message>
+      <source>Static Patching</source>
+      <translation type="unfinished">Static Patching</translation>
+    </message>
   </context>
   <context>
     <name>ShortcutDialog</name>
