0481188d2fb0ac32711dee68f957fdf5ff875487
georgemoralis <giorgosmrls@gmail.com>
2026-08-07 09:01:19 +0300

    New translations en_us.ts (Czech)
    [ci skip]

--- src/qt_gui/translations/cs_CZ.ts
+++ src/qt_gui/translations/cs_CZ.ts
@@ -2997,6 +2997,14 @@ Užitečné pro programy jako RenderDoc.</translation>
       <source>shadNet:\nCompatibility is very limited at the moment.\nYou can register at https://www.shadps4.net/shadnet/register/.</source>
       <translation type="unfinished">shadNet:\nCompatibility is very limited at the moment.\nYou can register at https://www.shadps4.net/shadnet/register/.</translation>
     </message>
+    <message>
+      <source>Red Zone Protection</source>
+      <translation type="unfinished">Red Zone Protection</translation>
+    </message>
+    <message>
+      <source>Static Patching</source>
+      <translation type="unfinished">Static Patching</translation>
+    </message>
   </context>
   <context>
     <name>ShortcutDialog</name>
