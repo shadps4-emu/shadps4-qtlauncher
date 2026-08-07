0147def6a815966546141f24829ea7c1a89e73cf
georgemoralis <giorgosmrls@gmail.com>
2026-08-07 09:01:55 +0300

    New translations en_us.ts (Persian)
    [ci skip]

--- src/qt_gui/translations/fa_IR.ts
+++ src/qt_gui/translations/fa_IR.ts
@@ -3022,6 +3022,14 @@ Immediate: فریم‌ها به محض آماده شدن فوراً روی صف�
       <source>shadNet:\nCompatibility is very limited at the moment.\nYou can register at https://www.shadps4.net/shadnet/register/.</source>
       <translation type="unfinished">shadNet:\nCompatibility is very limited at the moment.\nYou can register at https://www.shadps4.net/shadnet/register/.</translation>
     </message>
+    <message>
+      <source>Red Zone Protection</source>
+      <translation type="unfinished">Red Zone Protection</translation>
+    </message>
+    <message>
+      <source>Static Patching</source>
+      <translation type="unfinished">Static Patching</translation>
+    </message>
   </context>
   <context>
     <name>ShortcutDialog</name>
