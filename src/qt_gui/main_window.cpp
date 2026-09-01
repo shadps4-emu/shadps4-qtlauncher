faad4ec3cf5e31a4f67fb532e9e5496352238dbe
DanielSvoboda <28203966+DanielSvoboda@users.noreply.github.com>
2026-09-01 12:15:23 -0300

    Fix slow first game search in table view

--- src/qt_gui/main_window.cpp
+++ src/qt_gui/main_window.cpp
@@ -1039,21 +1039,18 @@ void MainWindow::StartGame() {
     StartGameWithArgs({});
 }
 
-bool isTable;
 void MainWindow::SearchGameTable(const QString& text) {
     if (isTableList) {
-        if (isTable != true) {
-            m_game_info->m_games = m_game_info->m_games_backup;
-            m_game_list_frame->PopulateGameList();
-            isTable = true;
-        }
-        for (int row = 0; row < m_game_list_frame->rowCount(); row++) {
-            QString game_name = QString::fromStdString(m_game_info->m_games[row].name);
-            bool match = (game_name.contains(text, Qt::CaseInsensitive)); // Check only in column 1
+        const int rowCount = m_game_list_frame->rowCount();
+
+        for (int row = 0; row < rowCount; ++row) {
+            const auto& game = m_game_info->m_games[row];
+            const bool match =
+                QString::fromStdString(game.name).contains(text, Qt::CaseInsensitive);
+
             m_game_list_frame->setRowHidden(row, !match);
         }
     } else {
-        isTable = false;
         m_game_info->m_games = m_game_info->m_games_backup;
         m_game_grid_frame->PopulateGameGrid(m_game_info->m_games, false);
 
