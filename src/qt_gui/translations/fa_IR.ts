848a9e1501b6e91ebe957946cdf340b2e994b865
georgemoralis <giorgosmrls@gmail.com>
2026-09-26 12:43:49 +0300

    New translations en_us.ts (Persian)
    [ci skip]

--- src/qt_gui/translations/fa_IR.ts
+++ src/qt_gui/translations/fa_IR.ts
@@ -2136,6 +2136,18 @@ Select an emulator version from the right panel.</source>
       <source>bootable files (*.bin *.elf *.oelf *.zar)</source>
       <translation type="unfinished">bootable files (*.bin *.elf *.oelf *.zar)</translation>
     </message>
+    <message>
+      <source>Error</source>
+      <translation type="unfinished">Error</translation>
+    </message>
+    <message>
+      <source>Rosetta 2 is not installed.
+
+Please install Rosetta 2 from Terminal using &apos;softwareupdate --install-rosetta&apos;.</source>
+      <translation type="unfinished">Rosetta 2 is not installed.
+
+Please install Rosetta 2 from Terminal using &apos;softwareupdate --install-rosetta&apos;.</translation>
+    </message>
   </context>
   <context>
     <name>QObject</name>
