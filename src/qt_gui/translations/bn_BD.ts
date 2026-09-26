b5087d4d48efde506f9c2c33450865b9cfaa4503
georgemoralis <giorgosmrls@gmail.com>
2026-09-26 12:43:05 +0300

    New translations en_us.ts (Bengali)
    [ci skip]

--- src/qt_gui/translations/bn_BD.ts
+++ src/qt_gui/translations/bn_BD.ts
@@ -2147,6 +2147,18 @@ Select an emulator version from the right panel.</source>
       <source>bootable files (*.bin *.elf *.oelf *.zar)</source>
       <translation>বুটযোগ্য ফাইল (*.bin *.elf *.oelf *.zar)</translation>
     </message>
+    <message>
+      <source>Error</source>
+      <translation type="unfinished">Error</translation>
+    </message>
+    <message>
+      <source>Rosetta 2 is not installed.
+
+Please install Rosetta 2 from Terminal using &apos;softwareupdate --install-rosetta&apos;.</source>
+      <translation type="unfinished">Rosetta 2 is not installed.
+
+Please install Rosetta 2 from Terminal using &apos;softwareupdate --install-rosetta&apos;.</translation>
+    </message>
   </context>
   <context>
     <name>QObject</name>
